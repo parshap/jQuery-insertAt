c81752bf47e07bcbfbbfac6c66da2e99ec3cb919
Parsha Pourkhomami <parshap+git@gmail.com>
2011-08-12 16:03:01 -0700

    Proof

--- jquery.insertAt.js
+++ jquery.insertAt.js
@@ -1,22 +1,10 @@
 /**
- * [ISC License](http://en.wikipedia.org/wiki/ISC_license)
+ * Copyright (c) Parsha Pourkhomami <parshap@gmail.com>
  *
- * Copyright (c) Year(s), Company or Person's Name <E-mail address>
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * Redistribution and use in source and compiled forms, with or without
+ * modification, are permitted under any circumstances. No warranty.
  */
 ;(function(global, $, undefined) {
-	// Insert content inside the
 	$.fn.insertAt = function(content, index) {
 
 		// Simple case if we are inserting at the beginning
@@ -24,8 +12,8 @@
 			return this.prepend(content);
 		}
 
-		// Otherwise we should the children of each DOM object in the set to
-		// determine the correct insert position for each one
+		// Otherwise we should examine the children of each DOM object in the
+		// set to determine the correct insert position for each one
 		return this.each(function() {
 			var $this = $(this),
 				el = $this.children().get(index);
